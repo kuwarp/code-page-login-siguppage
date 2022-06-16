b96f7f86b8a3e2a205b5dc0f57c418e6c37e77c0
Kuwar Pratap Singh <guru75000@gmail.com>
2022-06-16 13:40:18 +0530

    update-code

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,39 @@
-import React,{useState} from 'react'
-import {motion,AnimateSharedLayout} from "framer-motion"
+import React, { useState } from "react";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import link from "react-router-dom";
 
-import images from "../../assets/wave.gif"
-import  "./nav.css"
+import images from "../../assets/wave.gif";
+import "./nav.css";
 
-const Navbar = ({items,selectedId}) => {
-   
+const Navbar = ({ items, selectedId }) => {
   return (
     <>
       <div className="container">
+        <div className="logo">
+          <motion.div transition={{ duration: 0.5, ease: "easeOut" }}>
+            <img src={images} alt="logo" />
+          </motion.div>
+        </div>
 
-      <div className="logo">
-       <motion.div 
-       transition={{duration:0.5,ease:"easeOut"}}>
-         <img src={images} alt="logo" />
-       </motion.div></div>
-
-      <ul className="links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-          <li className="li-text" key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
-      </ul>
-      
+        <ul className="links">
+          {["home", "about", "work", "skills", "contact"].map((item) => (
+            <li className="li-text" key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="btn">
+        
+          <button className="btn-s" type="submit">
+         <a href="http://www.login.com">
+         Login/sign-up</a>
+          </button>
+          
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
